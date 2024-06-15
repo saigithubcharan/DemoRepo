a22e2fda257294de3a26054766ec4e4120bc65e7
saigithubcharan <battusaicharan@gmail.com>
2024-06-15 22:05:32 +0530

    one method added

--- src/main/java/demogroupid/Hello.java
+++ src/main/java/demogroupid/Hello.java
@@ -1,8 +1,12 @@
 package demogroupid;
 
 public class Hello {
-    void add(){
+    void add() {
         System.out.println(" Adding to github");
-        
+        //added by saicharan1
     }
+        void sub(){
+            System.out.println("added to git");
+        }
+
 }
